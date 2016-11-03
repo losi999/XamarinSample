88fcc112c848ef282bc8e766b862ee45fb9d5505
László Losonczi <laszlo_losonczi@epam.com>
2016-11-03 16:07:52 +0100

    add Windows Phone 8 project

---
+++ XamarinSample.WindowsPhone8/Services/NavigationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XamarinSample.BackEnd.ViewModel;
+using XamarinSample.Core.Services;
+using XamarinSample.WindowsPhone8.View;
+
+namespace XamarinSample.WindowsPhone8.Services {
+    public class NavigationService : GalaSoft.MvvmLight.Views.NavigationService, INavigationService {
+
+        public NavigationService() {
+            Configure(nameof(SecondViewModel), typeof(SecondPage));
+        }
+
+        public void NavigateToSecond(params object[] parameters) {
+            NavigateTo(nameof(SecondViewModel), parameters);
+        }
+    }
+}
